00d4eeb8b7fda3b25de189d3cbe91cb68a972f39
Derek Huang <djh458@stern.nyu.edu>
2024-04-21 20:47:09 -0400

    move C/C++ object/executable rules to rules.mk and include in top-level Makefile
    modified:   Makefile
        Move C/C++ object/executable rules to rules.mk and include rules.mk
    
    new file:   make/rules.mk
        Move C/C++ object/executable rules

--- Makefile
+++ Makefile
@@ -255,29 +255,8 @@ TFIGREEN =
 TNORMAL =
 endif
 
-# C object compile rule
-$(BUILDDIR)/%.o: %.c
-	@mkdir -p $(@D)
-	@printf "$(TFGREEN)Building C object $@$(TNORMAL)\n"
-	@$(CC) $(BASE_CFLAGS) $(CFLAGS) -MMD -MT $@ -MF $@.d -o $@ -c $<
-
-# C -fPIC object compile rule (shared library)
-$(BUILDDIR)/%.PIC.o: %.c
-	@mkdir -p $(@D)
-	@printf "$(TFGREEN)Building C object $@$(TNORMAL)\n"
-	@$(CC) -fPIC $(BASE_CFLAGS) $(CFLAGS) -MMD -MT $@ -MF $@.d -o $@ -c $<
-
-# C++ object compile rule
-$(BUILDDIR)/%.cc.o: %.cc
-	@mkdir -p $(@D)
-	@printf "$(TFGREEN)Building C++ object $@$(TNORMAL)\n"
-	@$(CXX) $(BASE_CXXFLAGS) $(CXXFLAGS) -MMD -MT $@ -MF $@.d -o $@ -c $<
-
-# C++ -fPIC object compile rule (shared library)
-$(BUILDDIR)/%.PIC.cc.o: %.cc
-	@mkdir -p $(@D)
-	@printf "$(TFGREEN)Building C++ object $@$(TNORMAL)\n"
-	@$(CXX) -fPIC $(BASE_CXXFLAGS) $(CXXFLAGS) -MMD -MT $@ -MF $@.d -o $@ -c $<
+# include C/C++ object and executable pattern rules
+include make/rules.mk
 
 # phony targets
 .PHONY: clean
---
+++ make/rules.mk
@@ -0,0 +1,32 @@
+# File: make/rules.mk
+# Author: Derek Huang
+# Brief: Makefile C/C++ object and executable pattern rules
+# Copyright: MIT License
+#
+# Note: Must include config.mk before use to define BASE_CFLAGS, BASE_CXXFLAGS,
+# TFGREEN, TFNORMAL, and BUILDDIR variables used in this file.
+#
+
+# C object compile rule
+$(BUILDDIR)/%.o: %.c
+	@mkdir -p $(@D)
+	@printf "$(TFGREEN)Building C object $@$(TNORMAL)\n"
+	@$(CC) $(BASE_CFLAGS) $(CFLAGS) -MMD -MT $@ -MF $@.d -o $@ -c $<
+
+# C -fPIC object compile rule (shared library)
+$(BUILDDIR)/%.PIC.o: %.c
+	@mkdir -p $(@D)
+	@printf "$(TFGREEN)Building C object $@$(TNORMAL)\n"
+	@$(CC) -fPIC $(BASE_CFLAGS) $(CFLAGS) -MMD -MT $@ -MF $@.d -o $@ -c $<
+
+# C++ object compile rule
+$(BUILDDIR)/%.cc.o: %.cc
+	@mkdir -p $(@D)
+	@printf "$(TFGREEN)Building C++ object $@$(TNORMAL)\n"
+	@$(CXX) $(BASE_CXXFLAGS) $(CXXFLAGS) -MMD -MT $@ -MF $@.d -o $@ -c $<
+
+# C++ -fPIC object compile rule (shared library)
+$(BUILDDIR)/%.PIC.cc.o: %.cc
+	@mkdir -p $(@D)
+	@printf "$(TFGREEN)Building C++ object $@$(TNORMAL)\n"
+	@$(CXX) -fPIC $(BASE_CXXFLAGS) $(CXXFLAGS) -MMD -MT $@ -MF $@.d -o $@ -c $<
